7b402e0c1f0a33bd1989570d18470f1262b4f618
KrishnaDagur2021 <90098220+KrishnaDagur2021@users.noreply.github.com>
2021-09-30 18:47:30 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,20 +11,76 @@ function preload(){
   climberImg = loadImage("climber.png");
   ghostImg = loadImage("ghost-standing.png");
   spookySound = loadSound("spooky.wav");
+  doorsGroup=new Group()
+  climbersGroup=new Group()
+invisibleBlockGroup=new Group()
+spookySound=loadSound("spooky.wav")
 }
 
 function setup() {
   createCanvas(600, 600);
+  spookySound.loop()
   tower = createSprite(300,300);
   tower.addImage("tower",towerImg);
   tower.velocityY = 1;
-  
+  ghost=createSprite(200,200,50,50)
+  ghost.scale=.3
+  ghost.addImage("ghost",ghostImg)
 }
 
 function draw() {
   background(200);
-  
+  if(gameState==="play"){
   if(tower.y > 400){
       tower.y = 300
     }
+    if(keyDown("left_arrow")){
+      ghost.x=ghost.x-3
+    }
+    if(keyDown("right_arrow")){
+      ghost.x=ghost.x+3
+    }
+    if(keyDown("space")){
+      ghost.velocityY=-5
+    }
+    ghost.velocityY=ghost.velocityY+.8
+if(climbersGroup.isTouching(ghost)){
+  ghost.velocityY=0
+}
+if(invisibleBlockGroup.isTouching(ghost)||ghost.y>600){
+  ghost.destroy()
+}
+    spawnDoors()
+    drawSprites()
+}
+if(gameState==="end"){
+  stroke("yellow")
+  fill("yellow")
+  textSize(30)
+  text("gameover",230,250)
+}}
+function spawnDoors(){
+  if(frameCount%240===0){
+    var door=createSprite(200,-50)
+    door.addImage(doorImg)
+    var climber=createSprite(200,10)
+    climber.addImage(climberImg)
+    var invisibleBlock=createSprite(200,15)
+    invisibleBlock.width=climber.width
+    invisibleBlock.height=2
+    door.x=Math.round(random(120,400))
+    door.velocityY=1
+    climber.velocityY=1
+    invisibleBlock.x=door.x
+    invisibleBlock.velocityY=1
+    ghost.depth=door.depth
+    ghost.depth+=1
+    door.lifetime=800
+    climber.lifetime=800
+    invisibleBlock.lifetime=800
+    doorsGroup.add(door)
+    climbersGroup.add(climber)
+    invisibleBlock.debug=true
+    invisibleBlockGroup.add(invisiblleBlock)
+  }
 }
